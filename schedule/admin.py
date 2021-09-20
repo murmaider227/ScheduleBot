843fd23818e8fb8c351998687f36c38f88fbf3d9
murmaider227 <gravitipro1437@gmail.com>
2021-09-20 18:46:44 +0300

    fix files

--- schedule/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-admin.site.register(Week)
-admin.site.register(Student)
-admin.site.register(Faculty)
-admin.site.register(Major)
-admin.site.register(Subject)
